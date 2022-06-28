85716c30d8607a3758bd074d2624306fbdca0cdb
rluver <skyd0608@gmail.com>
2022-06-28 17:21:42 +0900

    Add layer name

--- tensorflow/model/temporal_autoencoder.py
+++ tensorflow/model/temporal_autoencoder.py
@@ -11,8 +11,8 @@ class TemporalAutoencoder(Model):
     def __init__(self, config=model_config, **kwargs):
         super(TemporalAutoencoder, self).__init__(**kwargs)
         self.config = config
-        self.encoder = Encoder(config)
-        self.decoder = Decoder(config)
+        self.encoder = Encoder(config, name='encoder')
+        self.decoder = Decoder(config, name='decoder')
     
     def call(self, inputs, training=None):
         x = self.encoder(inputs)
@@ -21,8 +21,8 @@ class TemporalAutoencoder(Model):
 
 
 def build_temporal_autoencoder(config=model_config):
-    inputs = Input(shape=(config.time_seq, config.n_features))
-    encoder_output = Encoder(config)(input)
-    output = TemporalAutoencoder(config)(inputs)
+    inputs = Input(shape=(config.time_seq, config.n_features), name='input')
+    encoder_output = Encoder(config, name='latent_vector')(inputs)
+    output = TemporalAutoencoder(config, name='temporal_autoencoder')(inputs)
 
     return Model(inputs, [encoder_output, output])
